588763880546e5e742b36734399940fd2ca14343
Nicolai Parlog <nipa@codefx.org>
2021-03-30 21:03:01 +0200

    Show graph (yay! 🥳)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,76 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
+import ReactEChartsCore from "echarts-for-react/lib/core"
+import * as echarts from "echarts/core"
+import { GraphChart } from "echarts/charts"
+import { CanvasRenderer } from "echarts/renderers"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
-	const nodes = transform(data)
-	console.log(nodes)
+	const graph = createGraph(data)
+	const graphOptions = {
+		series: [
+			{
+				type: `graph`,
+				layout: `circular`,
+				...graph
+			},
+		],
+	}
+	echarts.use([GraphChart, CanvasRenderer])
+
 	return (
 		<Layout>
 			<SEO title="Home" />
+			<ReactEChartsCore echarts={echarts} option={graphOptions} />
 		</Layout>
 	)
 }
 
-const transform = (data) => {
-	const nodes: Node[] = data.graph.nodes.map((node: any): Node => ({
-		id: node.parent.name as String,
-		title: node.frontmatter.title as String,
-		parentId: node.frontmatter.parent as String,
-		html: node.html as String,
-	}))
-	return nodes.map((node: Node): Node => ({
-		...node,
-		parent: node.parentId && nodes.find(n => n.id == node.parentId),
-	}))
+const createGraph = (data: any): Graph => {
+	let nodes: Node[] = data.graph.nodes.map(
+		(node: any): Node => ({
+			id: node.parent.name as String,
+			name: node.frontmatter.title as String,
+			parentId: node.frontmatter.parent as String,
+			html: node.html as String,
+		})
+	)
+	nodes = nodes.map(
+		(node: Node): Node => ({
+			...node,
+			parent: node.parentId && nodes.find((n) => n.id == node.parentId),
+		})
+	)
+	const edges: Edge[] = nodes
+		.filter(node => node && node.parent)
+		.map((node) => ({
+			source: node.parent.id,
+			target: node.id,
+		}))
+
+	return { nodes, edges }
+}
+
+interface Graph {
+	nodes: Node[]
+	edges: Edge[]
 }
 
 interface Node {
 	id: String
-	title: String
+	name: String
 	parentId?: String
 	parent?: Node
 	html: String
+}
+
+interface Edge {
+	source: String
+	target: String
 }
 
 export default IndexPage
