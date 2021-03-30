218f1be06e397049459142f7d6cf9995efebf4ae
Nicolai Parlog <nipa@codefx.org>
2021-03-30 20:21:45 +0200

    Create Gatsby Starter + Remark

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({ data }) => {
+	return (
+		<Layout>
+			<SEO title="Home" />
+		</Layout>
+	)
+}
+
+export default IndexPage
