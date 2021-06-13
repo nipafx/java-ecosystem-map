f030d63dd569eb55cfcf2a6b1f151da22ee11fec
Nicolai Parlog <nipa@codefx.org>
2021-06-13 12:30:34 +0200

    Update README, LICENSE, etc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `Java Ecosystem Map`,
+				short_name: `java-map`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				background_color: `#101010`,
+				theme_color: `#436e90`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+				icon: `src/images/duke.png`, // This path is relative to the root of the site.
 			},
 		},
 		`gatsby-plugin-gatsby-cloud`,
