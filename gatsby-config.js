1307198e7ea2200d4fcbb4f12bcc1c5617ed3e3d
Nicolai Parlog <nipa@codefx.org>
2022-02-05 12:11:41 +0100

    Update Node to 17 and all dependencies to recent versions

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,6 @@ module.exports = {
 				display: `minimal-ui`,
 				icon: `src/images/duke.png`, // This path is relative to the root of the site.
 			},
-		},
-		`gatsby-plugin-gatsby-cloud`,
+		}
 	],
 }
